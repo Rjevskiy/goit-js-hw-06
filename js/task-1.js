4595f6849a332138a372edebacd5b3465e04a0dd
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-09-23 21:43:20 +0300

    s

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+
+
+
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+
+const evenNumbers = numbers.filter(numbers => numbers );
+const oddNumbers = numbers.filter(numbers => numbers % 2);
+
+
+
+// Значення змінної evenNumbers - це масив [24, 82, 36, 18, 52]
+
+// Значення змінної oddNumbers - це масив [17, 61, 47, 73]
